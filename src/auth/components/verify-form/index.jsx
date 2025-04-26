4c40ed66d25df493494a8f273b9e08c71a5b11cb
Harsh Gaur <hgaur491@gmail.com>
2025-04-26 13:43:56 +0530

    refactor verify form: rename 'otp' to 'code', update input placeholders, and enhance button loading state

--- src/auth/components/verify-form/index.jsx
+++ src/auth/components/verify-form/index.jsx
@@ -9,22 +9,23 @@ import { cn } from "@/lib/utils";
 import { useSearchParams } from "react-router";
 import { decodeEmail } from "@/auth/utils/hash";
 import NotFoundRoute from "@/routes/not-found";
+import { LoaderIcon } from "lucide-react";
 
 const verifySchema = z.object({
-    otp: z.string().length(6, "Verification otp must be 6 digits"),
+    code: z.string().length(6, "Verification code must be 6 digits"),
 });
 const emailSchema = z.object({
     email: z.string().email("Invalid email address"),
 });
 
 function maskEmail(email) {
-  const [username, domainWithTLD] = email.split("@");
-  const [domain, tld] = domainWithTLD.split(".");
+    const [username, domainWithTLD] = email.split("@");
+    const [domain, tld] = domainWithTLD.split(".");
 
-  const maskedUsername = username[0] + "•".repeat(Math.max(username.length - 1, 0));
-  const maskedDomain = domain[0] + "•".repeat(Math.max(domain.length - 1, 0));
+    const maskedUsername = username[0] + "•".repeat(Math.max(username.length - 1, 0));
+    const maskedDomain = domain[0] + "•".repeat(Math.max(domain.length - 1, 0));
 
-  return `${maskedUsername}@${maskedDomain}.${tld}`;
+    return `${maskedUsername}@${maskedDomain}.${tld}`;
 }
 
 export default function VerifyForm({ ...props }) {
@@ -34,11 +35,13 @@ export default function VerifyForm({ ...props }) {
 
     const form = useForm({
         resolver: zodResolver(verifySchema),
-        defaultValues: { otp: "" },
+        defaultValues: { code: "" },
     });
-
-    const onSubmit = (values) => {
-        console.log("Form Submitted:", values);
+    const { formState } = form;
+    const onSubmit = async (values) => {
+        const response = { ...values };
+        await new Promise((res) => setTimeout(res, 1000));
+        console.log("Form Submitted:", response); 
     };
     if (identifier == null || !parsedEmail.success) {
         return <NotFoundRoute />;
@@ -56,29 +59,27 @@ export default function VerifyForm({ ...props }) {
                 <h2 className="text-2xl font-semibold">Check your inbox</h2>
                 <div className="text-muted-foreground relative bottom-4 flex gap-2 flex-wrap">
                     Enter the code we just sent to
-                    <div>
-                        {parsedEmail?.data?.email ? maskEmail(parsedEmail.data.email) : "your email"}
-                    </div>
+                    <div>{parsedEmail?.data?.email ? maskEmail(parsedEmail.data.email) : "your email"}</div>
                 </div>
                 <FormField
                     control={form.control}
-                    name="otp"
+                    name="code"
                     render={({ field }) => (
                         <FormItem>
                             <FormLabel className={"inline-flex justify-between"}>OTP</FormLabel>
                             <FormControl>
-                                <Input type="text" placeholder="Enter 6 digit otp" {...field} />
+                                <Input type="text" placeholder="Enter 6 digit code" {...field} />
                             </FormControl>
                             <FormMessage />
                         </FormItem>
                     )}
                 />
 
-                <Button type="submit" className="w-full">
-                    Verify
+                <Button type="submit" className="w-full" disabled={formState.isSubmitting}>
+                    {formState.isSubmitting ? <LoaderIcon /> : "Continue"}
                 </Button>
                 <div className="text-center mt-4">
-                    Didn't receive a otp? <ResendOtpButton email={parsedEmail.data.email} />
+                    Didn't receive a code? <ResendOtpButton email={parsedEmail.data.email} />
                 </div>
             </form>
         </Form>
@@ -115,7 +116,7 @@ function ResendOtpButton({ email }) {
             disabled={isSending || timer > 0}
             className="font-bold text-primary hover:underline disabled:pointer-events-none disabled:opacity-50"
         >
-            {isSending ? "Sending..." : timer > 0 ? `Resend in ${timer}s` : "Resend otp"}
+            {isSending ? "Sending..." : timer > 0 ? `Resend in ${timer}s` : "Resend code"}
         </button>
     );
 }
